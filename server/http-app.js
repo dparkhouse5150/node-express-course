e7d2d852e377a329cf616304596d08ce65e07e66
darrellparkhouse5150 <86165927+darrellparkhouse5150@users.noreply.github.com>
2021-09-10 11:05:05 -0500

    simple http app

---
+++ server/http-app.js
@@ -0,0 +1,40 @@
+const http = require('http')
+const { readFileSync } = require('fs')
+
+const homePage = readFileSync('./navbar-app/index..html')
+const homeStyles = readFileSync('./navbar-app/styles.css')
+const homeImage = readFileSync('./navbar-app/logo.svg')
+const homeLogic = readFileSync('./navbar-app/browser-app.js')
+
+const server = http.createServer((req, res, ) => {
+    const url = req.url
+
+    console.log(url)
+
+    if (url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/html' })
+        res.write(homePage)
+        res.end()
+    } else if (url === '/about') {
+        res.writeHead(200, { 'Content-Type': 'text/html' })
+        res.write('<h1>About Page</h1>')
+        res.end()
+    } else if (url === '/styles.css') {
+        res.writeHead(200, { 'Content-Type': 'text/css'})
+        res.write(homeStyles)
+        res.end()
+    } else if (url === 'logo.svg') {
+        res.writeHead(200, { 'Content-Type': 'image/svg+xml' })
+        res.write(homeImage)
+        res.end()
+    } else if (url === '/brower-app.js') {
+        res.writeHead(200, { 'Content-Type': 'text/javascript'})
+        res.write(homeLogic)
+        res.end()
+    } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' })
+        res.write('<h1>page not found</h1>')
+    }
+})
+
+server.listen(5000)
