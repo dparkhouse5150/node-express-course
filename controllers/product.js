c3b0c5cb7f00f88df28adcb1627f241334be5b44
darrellparkhouse5150 <86165927+darrellparkhouse5150@users.noreply.github.com>
2021-09-10 11:11:18 -0500

    routes for the product component

---
+++ controllers/product.js
@@ -0,0 +1,4 @@
+const Product = require('../models/Products')
+const { StatusCodes } = require('http-status-code')
+const CustomError = require('../models/CustomError')
+const path = require('path')
